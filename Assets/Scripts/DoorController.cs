ec8ecf0c652057529562962d5267b335227ad4a8
Goose <niken54@mail.ru>
2021-02-17 17:45:34 +0300

    Added door animation, new level, key help

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -6,22 +6,56 @@ public class DoorController : MonoBehaviour
 {
     private BoxCollider2D mainCollider;
     private SpriteRenderer sr;
+    //private Animator anim;
+
+    //private bool isOpenning;
+    //private bool isClosing;
+
+    private Coroutine curCor;
 
     private void Start()
     {
         mainCollider = GetComponent<BoxCollider2D>();
         sr = GetComponent<SpriteRenderer>();
+        //anim = GetComponent<Animator>();
     }
 
     public void Open()
     {
-        mainCollider.enabled = false;
-        sr.enabled = false;
+        //if (isOpenning)
+        //return;
+
+        //mainCollider.enabled = false;
+        //sr.enabled = false;
+        //anim.SetTrigger("Close");
+        //isOpenning = true;
+        StartCoroutine(OnOpen());
     }
 
     public void Close()
     {
-        mainCollider.enabled = true;
-        sr.enabled = true;
+        //if (isClosing)
+        //    return;
+
+        ////mainCollider.enabled = true;
+        ////sr.enabled = true;
+        //anim.SetTrigger("Open");
+        //isClosing = true;
+        StartCoroutine(OnClose());
+    }
+
+    private IEnumerator OnOpen()
+    {
+        yield return null;
+    }
+
+    private IEnumerator OnClose()
+    {
+        yield return null;
+    }
+
+    private void GetReadyToCor()
+    {
+
     }
 }
